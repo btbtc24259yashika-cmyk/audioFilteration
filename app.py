5dad9c872b69b37e2adde33a5d3b5b2d4534a2f0
monday_jannineteen <ayashika942@gmail.in>
2026-03-06 22:24:11 +0530

    added: render config

--- app.py
+++ app.py
@@ -119,6 +119,6 @@ def process_audio():
 def outputs(filename):
     return send_from_directory(OUTPUT_FOLDER, filename)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
